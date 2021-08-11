3448024dc406838a1331d8753f9b0069acb4d502
Krot Eval <38257723+4n70w4@users.noreply.github.com>
2021-08-11 18:05:56 +0300

    Update setup.sh
    It's a very bad idea to do this. Very bad. Very bad. Think of another way to do it.

--- setup.sh
+++ setup.sh
@@ -61,10 +61,6 @@ setup_compose() {
     GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
     wget https://raw.githubusercontent.com/getredash/setup/${GIT_BRANCH}/data/docker-compose.yml
     sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
-    echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
-    echo "export COMPOSE_FILE=/opt/redash/docker-compose.yml" >> ~/.profile
-    export COMPOSE_PROJECT_NAME=redash
-    export COMPOSE_FILE=/opt/redash/docker-compose.yml
     sudo docker-compose run --rm server create_db
     sudo docker-compose up -d
 }
