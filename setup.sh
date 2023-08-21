98b8c51f3505921a4a4b6494913c679145c7e3c1
snickerjp <snickerjp@gmail.com>
2023-08-21 17:21:24 +0000

    replace /opt/redash to $REDASH_BASE_PATH from #35

--- setup.sh
+++ setup.sh
@@ -63,16 +63,16 @@ setup_compose() {
   REQUESTED_CHANNEL=stable
   LATEST_VERSION=$(curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL" | json_pp | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}')
 
-  cd "$REDASH_BASE_PATH"
-  GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-  curl -OL https://raw.githubusercontent.com/getredash/setup/"$GIT_BRANCH"/data/docker-compose.yml
-  sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
-  echo "export COMPOSE_PROJECT_NAME=redash" >>~/.profile
-  echo "export COMPOSE_FILE=$REDASH_BASE_PATH/docker-compose.yml" >>~/.profile
-  export COMPOSE_PROJECT_NAME=redash
-  export COMPOSE_FILE=$REDASH_BASE_PATH/docker-compose.yml
-  sudo docker-compose run --rm server create_db
-  sudo docker-compose up -d
+    cd $REDASH_BASE_PATH
+    GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
+    curl -OL https://raw.githubusercontent.com/getredash/setup/${GIT_BRANCH}/data/docker-compose.yml
+    sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
+    echo "export COMPOSE_PROJECT_NAME=redash" >> ~/.profile
+    echo "export COMPOSE_FILE=$REDASH_BASE_PATH/docker-compose.yml" >> ~/.profile
+    export COMPOSE_PROJECT_NAME=redash
+    export COMPOSE_FILE=$REDASH_BASE_PATH/docker-compose.yml
+    sudo docker-compose run --rm server create_db
+    sudo docker-compose up -d
 }
 
 install_docker
