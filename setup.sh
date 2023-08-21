20d4c3c127364b24d8ddd8b1cbf3fbf743a058f3
snickerjp <snickerjp@gmail.com>
2023-08-21 17:18:20 +0000

    remove white space

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
-# This script setups dockerized Redash on Ubuntu 18.04.
+# This script setups dockerized Redash on Ubuntu 20.04.
 set -eu
 
 REDASH_BASE_PATH=/opt/redash
 
-install_docker() {
-  # Install Docker
-  export DEBIAN_FRONTEND=noninteractive
-  sudo apt-get -qqy update
-  DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-  sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt-get update && sudo apt-get -y install docker-ce
+install_docker(){
+    # Install Docker
+    export DEBIAN_FRONTEND=noninteractive
+    sudo apt-get -qqy update
+    DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+    sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update && sudo apt-get -y install docker-ce
 
   # Install Docker Compose
   sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-"$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
