69e01877fe1a065d53e1018cb607256a54ed9ff7
Justin Clift <justin@postgresql.org>
2024-03-21 22:05:26 +1000

    Use the Redash version in the git repo
    The call to https://version.redash.io isn't useful any more, as
    the returned version string is extremely outdated.
    
    For now, lets just use the version string in the GitHub repo.  We
    can figure out something more dynamic later on if it turns out to
    be needed.

--- setup.sh
+++ setup.sh
@@ -62,13 +62,9 @@ EOF
 }
 
 setup_compose() {
-  REQUESTED_CHANNEL=stable
-  LATEST_VERSION=$(curl -s "https://version.redash.io/api/releases?channel=$REQUESTED_CHANNEL" | json_pp | grep "docker_image" | head -n 1 | awk 'BEGIN{FS=":"}{print $3}' | awk 'BEGIN{FS="\""}{print $1}')
-
   cd "$REDASH_BASE_PATH"
   GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
   curl -OL https://raw.githubusercontent.com/getredash/setup/"$GIT_BRANCH"/data/docker-compose.yml
-  sed -ri "s/image: redash\/redash:([A-Za-z0-9.-]*)/image: redash\/redash:$LATEST_VERSION/" docker-compose.yml
   echo "export COMPOSE_PROJECT_NAME=redash" >>~/.profile
   echo "export COMPOSE_FILE=$REDASH_BASE_PATH/docker-compose.yml" >>~/.profile
   export COMPOSE_PROJECT_NAME=redash
