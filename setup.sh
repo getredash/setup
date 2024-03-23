b9ee472e57b0bb78e81f0bb187e5d46e4cb02a47
Justin Clift <justin@postgresql.org>
2024-03-23 16:20:28 +1000

    Add support for Debian, add friendlier text output

--- setup.sh
+++ setup.sh
@@ -1,37 +1,72 @@
-#!/usr/bin/env bash
-# This script setups dockerized Redash on Ubuntu 20.04.
+#!/usr/bin/env sh
+# This script sets up dockerized Redash on Debian 12.x, Ubuntu 20.04, and Ubuntu 22.04
 set -eu
 
 REDASH_BASE_PATH=/opt/redash
 
-install_docker() {
-  # Install Docker
+# Ensure the script is being run as root
+ID=$(id -u)
+if [ "0$ID" -ne 0 ]
+  then echo "Please run this script as root"
+  exit
+fi
+
+# Ensure the script is running on something it can work with
+if [ ! -f /etc/os-release ]; then
+  echo "Unknown Linux distribution.  This script presently works only on Debian and Ubuntu"
+  exit
+fi
+
+install_docker_debian() {
+  echo "** Installing Docker (Debian) **"
+
   export DEBIAN_FRONTEND=noninteractive
-  sudo apt-get -qqy update
-  DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-  sudo apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen gnupg
+  apt-get -qqy update
+  DEBIAN_FRONTEND=noninteractive apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+  apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen gnupg
+
+  # Add Docker GPG signing key
   if [ ! -f "/etc/apt/keyrings/docker.gpg" ]; then
-    sudo install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
   fi
-  echo \
-    "deb [arch=""$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    ""$(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-    sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-  sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-  # Install Docker Compose
-  sudo ln -sfv /usr/libexec/docker/cli-plugins/docker-compose /usr/local/bin/docker-compose
+  # Add Docker download repository to apt
+  cat <<EOF >/etc/apt/sources.list.d/docker.list
+deb [arch=""$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable
+EOF
+  apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+}
 
-  # Allow current user to run Docker commands
-  sudo usermod -aG docker "$USER"
+install_docker_ubuntu() {
+  echo "** Installing Docker (Ubuntu) **"
+
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -qqy update
+  DEBIAN_FRONTEND=noninteractive sudo -E apt-get -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+  apt-get -yy install apt-transport-https ca-certificates curl software-properties-common pwgen gnupg
+
+  # Add Docker GPG signing key
+  if [ ! -f "/etc/apt/keyrings/docker.gpg" ]; then
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+  fi
+
+  # Add Docker download repository to apt
+  cat <<EOF >/etc/apt/sources.list.d/docker.list
+deb [arch=""$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable
+EOF
+  apt-get update && apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 }
 
 create_directories() {
+  echo "** Creating $REDASH_BASE_PATH directory structure for Redash **"
+
   if [ ! -e "$REDASH_BASE_PATH" ]; then
-    sudo mkdir -p "$REDASH_BASE_PATH"
-    sudo chown "$USER:" "$REDASH_BASE_PATH"
+    mkdir -p "$REDASH_BASE_PATH"
+    chown "$USER:" "$REDASH_BASE_PATH"
   fi
 
   if [ ! -e "$REDASH_BASE_PATH"/postgres-data ]; then
@@ -39,7 +74,9 @@ create_directories() {
   fi
 }
 
-create_config() {
+create_env() {
+  echo "** Creating Redash environment file **"
+
   if [ -e "$REDASH_BASE_PATH"/env ]; then
     rm "$REDASH_BASE_PATH"/env
     touch "$REDASH_BASE_PATH"/env
@@ -62,18 +99,56 @@ EOF
 }
 
 setup_compose() {
+  echo "** Creating Redash Docker compose file **"
+
   cd "$REDASH_BASE_PATH"
   GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-  curl -OL https://raw.githubusercontent.com/getredash/setup/"$GIT_BRANCH"/data/docker-compose.yml
-  echo "export COMPOSE_PROJECT_NAME=redash" >>~/.profile
-  echo "export COMPOSE_FILE=$REDASH_BASE_PATH/docker-compose.yml" >>~/.profile
+  curl -fsSOL https://raw.githubusercontent.com/getredash/setup/"$GIT_BRANCH"/data/compose.yaml
+  curl -fsSOL https://raw.githubusercontent.com/getredash/setup/"$GIT_BRANCH"/redash_make_default.sh
+  sed -i "s|__BASE_PATH__|${REDASH_BASE_PATH}|" redash_make_default.sh
+  chmod +x redash_make_default.sh
   export COMPOSE_PROJECT_NAME=redash
-  export COMPOSE_FILE="$REDASH_BASE_PATH"/docker-compose.yml
-  sudo docker-compose run --rm server create_db
-  sudo docker-compose up -d
+  export COMPOSE_FILE="$REDASH_BASE_PATH"/compose.yaml
+
+  echo "** Initialising fresh Redash database **"
+  docker compose run --rm server create_db
+
+  echo
+  echo "*********************"
+  echo "** Starting Redash **"
+  echo "*********************"
+  docker compose up -d
 }
 
-install_docker
+echo
+echo "Redash installation script. :)"
+echo
+
+# Run the distro specific Docker installation
+DISTRO=$(grep '^ID=' /etc/os-release | cut -d '=' -f 2)
+if [ "x${DISTRO}" = "xdebian" ]; then
+  install_docker_debian
+elif [ "x${DISTRO}" = "xubuntu" ]; then
+  install_docker_ubuntu
+else
+  echo "This doesn't seem to be a Debian nor Ubuntu system, so this script doesn't know how to add Docker to it."
+  echo
+  echo "Please contact the Redash project via GitHub and ask about getting support added (or add it yourself and let us know). :)"
+  echo
+  exit
+fi
+
+# Do the things that aren't distro specific
 create_directories
-create_config
+create_env
 setup_compose
+
+echo
+echo "Redash has been installed and is ready for configuring at http://$(hostname -f):5000"
+echo
+
+echo "If you want Redash to be your default Docker Compose project when you login to this server"
+echo "in future, then please run $REDASH_BASE_PATH/redash_make_default.sh"
+echo
+echo "That will set some Docker specific environment variables just for Redash.  If you"
+echo "already use Docker Compose on this computer for other things, you should probably skip it."
