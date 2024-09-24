f50f70a31ac8f9805873a23a34d1d451c1ca8d4d
Justin Clift <justin@postgresql.org>
2024-09-24 18:05:12 +1000

    Fix breakage of --overwrite without an existing installation

--- setup.sh
+++ setup.sh
@@ -234,7 +234,7 @@ create_env() {
 
     # Move any existing environment file out of the way
     TIMESTAMP_NOW=$(date +'%Y.%m.%d-%H.%M')
-    mv -f "${REDASH_BASE_PATH}/env" "${REDASH_BASE_PATH}/env.old.${TIMESTAMP_NOW}"
+    mv -f "${REDASH_BASE_PATH}/env" "${REDASH_BASE_PATH}/env.old-${TIMESTAMP_NOW}"
   fi
 
   echo "Generating brand new environment file"
@@ -257,7 +257,7 @@ setup_compose() {
 
   cd "$REDASH_BASE_PATH"
   GIT_BRANCH="${REDASH_BRANCH:-master}" # Default branch/version to master if not specified in REDASH_BRANCH env var
-  if [ "x$OVERWRITE" = "xyes" ]; then
+  if [ "x$OVERWRITE" = "xyes" -a -e compose.yaml ]; then
     mv -f compose.yaml compose.yaml.old
   fi
   curl -fsSOL https://raw.githubusercontent.com/getredash/setup/"$GIT_BRANCH"/data/compose.yaml
